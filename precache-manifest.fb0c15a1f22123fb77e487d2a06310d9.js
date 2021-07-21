6f7d793ecbc0917ac38024e03cc6ee3180eeacba
Kulyk Oleksandr <70694467+akylik@users.noreply.github.com>
2021-07-21 12:40:18 +0300

    Updates

---
+++ precache-manifest.fb0c15a1f22123fb77e487d2a06310d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f53190d0a1dbeba52a4b788543cf65",
+    "url": "/react-movies_finder/index.html"
+  },
+  {
+    "revision": "36bf210e8c550873f4ae",
+    "url": "/react-movies_finder/static/css/main.5d4f45ab.chunk.css"
+  },
+  {
+    "revision": "c4773edcc5ef7b415724",
+    "url": "/react-movies_finder/static/js/0.1bee2e0a.chunk.js"
+  },
+  {
+    "revision": "83f05d58c64a8d40002f",
+    "url": "/react-movies_finder/static/js/5.9ba20ea5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movies_finder/static/js/5.9ba20ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a033689fab0f826ba9e",
+    "url": "/react-movies_finder/static/js/home-view.6b0cac77.chunk.js"
+  },
+  {
+    "revision": "36bf210e8c550873f4ae",
+    "url": "/react-movies_finder/static/js/main.c7130706.chunk.js"
+  },
+  {
+    "revision": "0101dfe89f0a680936c1",
+    "url": "/react-movies_finder/static/js/movies-view.c03bca87.chunk.js"
+  },
+  {
+    "revision": "ecf4f60573915fa45d5f",
+    "url": "/react-movies_finder/static/js/runtime-main.d6ad6df6.js"
+  }
+]);
